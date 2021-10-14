5203c8d99720ca21ff6ca08e957b484ce649c8e8
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-14 10:25:29 +0200

    First unittest - excercise

--- unittest_basic/first_unittest.py
+++ unittest_basic/first_unittest.py
@@ -1,5 +1,12 @@
 import unittest
 
 class TestClass1(unittest.TestCase):
-    def test_case_1(self,):
+    def test_case_1(self):
         self.assertEqual( 'aws'.upper(), 'AWS')
+
+    def test_case_2(self):
+        self.assertTrue('Apple'.islower())
+
+class TestClass2(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(1, '1')
